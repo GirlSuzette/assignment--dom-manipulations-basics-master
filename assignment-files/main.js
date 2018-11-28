c765767f4a66c71db34feba47008628ace97138e
GirlSuzette <trinitysuzette_marpis@hotmail.com>
2018-11-28 09:41:25 -0600

    first commitd2

--- assignment-files/main.js
+++ assignment-files/main.js
@@ -1,19 +1,19 @@
 document
   .querySelector("#inject-html button")
-  .addEventListener("click", function() {
+  .addEventListener("click", function () {
     // TASK #1
     var answerBoxEl = document.querySelector("#inject-html .answer-box");
     answerBoxEl.innerHTML = "<h2>Here is some text for you!!</h2>";
   });
 
-document.querySelector("#double button").addEventListener("click", function() {
+document.querySelector("#double button").addEventListener("click", function () {
   // TASK #2
   var answerPTagWithValue = document.querySelector("#compoundInvestment");
   // you do the rest
   answerPTagWithValue.innerHTML = answerPTagWithValue.textContent * 2;
 });
 
-document.querySelector("#blow-up button").addEventListener("click", function() {
+document.querySelector("#blow-up button").addEventListener("click", function () {
   // TASK #3
   var circle = document.querySelector(".circle-red");
 
@@ -26,41 +26,41 @@ document.querySelector("#blow-up button").addEventListener("click", function() {
   }
 });
 
-document.querySelector("#remove button").addEventListener("click", function() {
+document.querySelector("#remove button").addEventListener("click", function () {
   // TASK #4
   var listInactive = document.querySelectorAll(".inactive");
 
-  listInactive.forEach(function(listElement) {
+  listInactive.forEach(function (listElement) {
     listElement.remove();
   });
 });
 
 document
   .querySelector("#reverse-squares button")
-  .addEventListener("click", function() {
+  .addEventListener("click", function () {
     // TASK #5
     var span = [];
     var fatherBox = document.querySelector("#squares-box");
     var listReverse = document.querySelectorAll(".square");
 
-    listReverse.forEach(function(itm) {
+    listReverse.forEach(function (itm) {
       span.push(itm);
       itm.remove();
     });
-    span.reverse().forEach(function(itm) {
+    span.reverse().forEach(function (itm) {
       fatherBox.appendChild(itm);
     });
   });
 
 document
   .querySelector("#pig-latin button")
-  .addEventListener("click", function() {
+  .addEventListener("click", function () {
     // TASK #6
 
     var list = document.querySelectorAll("#tasks li");
     var fatherBoxList = document.querySelector("#tasks");
 
-    list.forEach(function(itm) {
+    list.forEach(function (itm) {
       var text = itm.textContent;
       var reverse = text
         .split("")
@@ -73,14 +73,13 @@ document
 
 document
   .querySelector("#cycle-image button")
-  .addEventListener("click", function() {
-    //TASK #7
-    function changeImage() {
-      var image = document.querySelector("myImage");
-      if (image.src.match("bulbon")) {
-        image.src = "pic_bulboff.png";
-      } else {
-        image.src = "pic_bulbon.png";
-      }
+  .addEventListener("click", function () {
+    //TASK #
+    var imagen = document.querySelector("#city-img");
+    if (imagen > 10) {
+      count++
+      imagen.innerHTML = "./images/city-photo-" + count++ + ".jpg"
     }
+
+
   });
